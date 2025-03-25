7f330a7101da58126d665c91429973e44e8100f5
Sai Sashreek D <saisashreekd@gmail.com>
2025-03-25 11:48:37 +0530

    Updated Home Page Component

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,50 +1,38 @@
 import React from "react";
 import {
-  Header,
-  Footer,
   Intro,
   Services,
   About,
   Counter,
   WhatWeDo,
   HowWeWork,
-  ContactUs
+  ContactUs,
 } from "./Components/index";
 
 export default function Home() {
   return (
     <>
-      {/* Header Start */}
-      <Header />
-      {/* Header End */}
-      {/* Main Start */}
-      <main>
-        {/* Intro Start */}
-        <Intro />
-        {/* Intro End */}
-        {/* Service Start */}
-        <Services />
-        {/* Service End */}
-        {/* About Start */}
-        <About />
-        {/* About End */}
-        {/* Counter Start */}
-        <Counter />
-        {/* Counter End */}
-        {/* What We Do Start */}
-        <WhatWeDo />
-        {/* What We Do End */}
-        {/* How We Work Start */}
-        <HowWeWork />
-        {/* How We Work End */}
-        {/* Contact Us Start */}
-        <ContactUs />
-        {/* Contact Us End */}
-      </main>
-      {/* Main End */}
-      {/* Footer Start */}
-      <Footer />
-      {/* Footer End */}
+      {/* Intro Start */}
+      <Intro />
+      {/* Intro End */}
+      {/* Service Start */}
+      <Services />
+      {/* Service End */}
+      {/* About Start */}
+      <About />
+      {/* About End */}
+      {/* Counter Start */}
+      <Counter />
+      {/* Counter End */}
+      {/* What We Do Start */}
+      <WhatWeDo />
+      {/* What We Do End */}
+      {/* How We Work Start */}
+      <HowWeWork />
+      {/* How We Work End */}
+      {/* Contact Us Start */}
+      <ContactUs />
+      {/* Contact Us End */}
     </>
   );
 }
